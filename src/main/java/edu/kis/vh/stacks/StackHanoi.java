9a574fa5d4b744f6aa18cd1678d210fd7fa933e4
misckq <misckq@gmail.com>
2017-03-02 15:21:20 +0100

    Poprawiono błędy związane z formatowaniem kodu źródłowego we wszystkich klasach

--- src/main/java/edu/kis/vh/stacks/StackHanoi.java
+++ src/main/java/edu/kis/vh/stacks/StackHanoi.java
@@ -4,14 +4,14 @@ public class StackHanoi extends stack {
 
 int totalRejected = 0;
 
-	public int reportRejected() {
-	return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void push(int in) {
-	if (!isEmpty() && in > top())
-		totalRejected++;
-		else
-			super.push(in);
-	}
+    public void push(int in) {
+    if (!isEmpty() && in > top())
+        totalRejected++;
+    else
+        super.push(in);
+    }
 }
