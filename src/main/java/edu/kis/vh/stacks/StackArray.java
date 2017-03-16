dea1d7058d4e21551ac9553edad9041f606111fe
misckq <misckq@gmail.com>
2017-03-16 14:39:49 +0100

    Dodano klasę StackArray

--- src/main/java/edu/kis/vh/stacks/StackArray.java
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -1,13 +1,12 @@
 package edu.kis.vh.stacks;
 
-public class Stack {
+public class StackArray {
 	
     private static final int EMPTY_STACK_VALUE = -1;
     private static final int SIZE = 12;
     
     private final int[] ITEMS = new int[SIZE];
     private int total = EMPTY_STACK_VALUE;
-
 
     public void push(int i) {
         if (!isFull()) {
